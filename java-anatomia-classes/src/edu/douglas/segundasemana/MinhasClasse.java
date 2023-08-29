a98e44ae5b0971a19f38ef77cc728c752978525d
Douglas-Augusto <douglas.full.ti@gmail.com>
2023-08-29 16:09:07 -0300

    28/08/2023-16:09

---
+++ java-anatomia-classes/src/edu/douglas/segundasemana/MinhasClasse.java
@@ -0,0 +1,16 @@
+package edu.douglas.segundasemana;
+public class MinhasClasse {
+    
+    public static void main (String [] args){
+        String primeiroNome = "Douglas";
+        String segundoNome = "Augusto";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        
+        System.out.println(nomeCompleto);
+        
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
