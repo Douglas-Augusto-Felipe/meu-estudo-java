7584d2d33997189f9a498b1af245df1040b8cf4b
Douglas-Augusto <douglas.full.ti@gmail.com>
2023-08-30 17:35:24 -0300

    30/08/2023-15:35

---
+++ java-anatomia-classes/src/edu/douglas/tiposoperadores/tiposOperadores2.java
@@ -0,0 +1,16 @@
+package douglas.tiposoperadores;
+
+
+public class tiposOperadores2 {
+    public static void main(String[] args){
+     int numero = 5;
+
+     numero =  -numero;
+     
+     System.out.println(numero);
+
+     numero = numero* -1;
+
+     System.out.println(numero);
+    }
+}
