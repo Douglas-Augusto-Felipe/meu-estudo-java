5bf4b58a2072f622a7d081402ce2605c1ce82f25
Douglas-Augusto <douglas.full.ti@gmail.com>
2023-09-04 19:46:40 -0300

    04/09/2023-19:46

---
+++ java-anatomia-classes/src/edu/douglas/ExemploFor/exemploForCaneirinho.java
@@ -0,0 +1,10 @@
+package douglas.ExemploFor;
+
+public class exemploForCaneirinho {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+            System.out.println("contando carneirinhos " + carneirinhos);
+        }
+
+    }
+}
