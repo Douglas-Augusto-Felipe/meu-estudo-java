6c2409b805a479c7528dada6e3b0e254561cec33
Douglas Augusto <116586173+Douglas-Augusto-Felipe@users.noreply.github.com>
2023-09-08 16:25:36 -0300

    Add files via upload

---
+++ java-anatomia-classes/src/edu/douglas/tiposoperadores/tiposOperadores3.java
@@ -0,0 +1,17 @@
+package douglas.tiposoperadores;
+
+
+public class tiposOperadores3 {
+    public static void main(String[] args){
+        // int numero = 5;
+        // //x repetição
+        // numero++;
+        // // numero = numero +1;
+
+        // System.out.println(++numero);
+
+        boolean variavel = true;
+        
+        System.out.println(!variavel);
+    }
+}
